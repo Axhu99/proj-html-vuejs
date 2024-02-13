2139349543c056bba170267c5c0ce994462d1721
FrancescoM95 <mon95fran@gmail.com>
2024-02-13 18:11:07 +0100

    added bestBlogs section

---
+++ src/components/blogsSection/BlogsSection.vue
@@ -0,0 +1,17 @@
+<script>
+import NumbersSection from './NumbersSection.vue';
+import BlogsMain from './BlogsMain.vue';
+export default {
+    name: 'BlogsSection',
+    components: { NumbersSection, BlogsMain }
+}
+
+</script>
+
+<template>
+    <section id="blogs-section">
+        <NumbersSection />
+        <BlogsMain />
+    </section>
+</template>
+
---
+++ src/components/blogsSection/NumbersCard.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    name: 'NumbersCard',
+    props: {
+        blog: Object
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="numbers-card">
+        <h5>{{ blog.title }}</h5>
+        <div class="number">{{ blog.value }}</div>
+        <h6>{{ blog.subtitle }}</h6>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.numbers-card {
+    height: 400px;
+    flex-basis: 25%;
+    text-align: center;
+    padding: 40px;
+    border-left: 1px solid rgba(211, 211, 211, 0.438);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+
+    h5 {
+        font-size: 24px;
+        color: #3F3A64;
+        font-weight: 700;
+        padding: 0 20px;
+    }
+
+    h6 {
+        color: #8C89A2;
+        font-weight: 700;
+    }
+
+    .number {
+        font-size: 64px;
+        font-weight: 800;
+        color: #20AD96;
+    }
+}
+</style>
