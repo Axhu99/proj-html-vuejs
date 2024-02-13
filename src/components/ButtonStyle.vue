78ea5a4f387f3931b4c6a194debc94e4842edd4c
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-13 09:47:12 +0000

    Added Button Style

---
+++ src/components/ButtonStyle.vue
@@ -0,0 +1,40 @@
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+export default {
+    name: 'ButtonStyle',
+    components: {
+        FontAwesomeIcon
+    },
+    props: {
+        buttonText: {
+            type: String,
+            default: 'Get started now',
+        },
+        icon: {
+            type: Array,
+            required: true,
+        }
+    }
+};
+</script>
+
+<template>
+    <div>
+        <button type="button" class="btn"> <font-awesome-icon :icon="icon" /> {{ buttonText }}</button>
+    </div>
+</template>
+
+<style scoped>
+.btn {
+    background-color: #20AD96;
+    width: 250px;
+    padding: 13px 12px;
+    color: white;
+    font-weight: bold;
+}
+</style>
+
+
+<!-- <font-awesome-icon :icon="['fas', 'download']" /> -->
+<!--<font-awesome-icon :icon="['fas', 'arrow-right']" /> -->
