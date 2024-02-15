30c47d141eee0abdd9890f89602c30cc7950f11d
FrancescoM95 <mon95fran@gmail.com>
2024-02-15 11:39:45 +0100

    added jumbotron effects

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -14,7 +14,7 @@ export default {
 <template>
     <div class="container">
         <div class="row py-5">
-            <div class="col-6 d-flex align-itens-center">
+            <div class="col-5 d-flex align-itens-center">
                 <div class="jumbotronText">
                     <h1 class="titleJT">Distant learning for further expansion</h1>
                     <p class="mt-2  mb-5">Learning is a life-long journey that in fact we never find the terminate stop.
@@ -26,9 +26,15 @@ export default {
                                 :icon="['fas', 'arrow-right']" /></span></p>
                 </div>
             </div>
-            <div class="col-6">
-                <img src="/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt=""
+            <div class="col-6 offset-1 position-relative">
+                <img src="/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="jumbotron"
                     class="rounded-circle">
+                <div class="effects-div">
+                    <img src="/img/maxcoach-shape-08.png" alt="effect">
+                    <img src="/img/maxcoach-shape-08.png" alt="effect">
+                </div>
+                <img src="/img/maxcoach-shape-07-150x150.png" alt="effect" class="effect-shape">
+                <img src="/img/maxcoach-shape-02.png" alt="effect" class="effect-circle">
             </div>
             <div class="col-10 offset-1 mt-5">
                 <ul class="d-flex justify-content-between align-items-center list-unstyled">
@@ -73,5 +79,28 @@ li {
 
 li:hover {
     filter: none;
+}
+
+.effects-div {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    position: absolute;
+    top: 20%;
+    left: -5%;
+    z-index: -1;
+}
+
+.effect-shape {
+    position: absolute;
+    bottom: 5%;
+    right: 0;
+    z-index: -1;
+}
+
+.effect-circle {
+    position: absolute;
+    top: 15%;
+    right: -10%;
 }
 </style>
